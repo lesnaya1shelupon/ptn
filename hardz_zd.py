71268d1738fa0058e007e489cb3fb1cd81a1c7f6
lesnaya1shelupon <tookdd55@gmail.com>
2022-05-03 19:37:15 +0300

    dop.zad v2

--- hardz_zd.py
+++ hardz_zd.py
@@ -4,11 +4,8 @@ a = int(input("a1"))
 b2 = int(input("b2"))
 b = int(input("b1"))
 ab = a + b
-c2 = ab // 10
-a2b2 = a2 + b2 + c2
-c3 = a2b2 // 10
-cc = a2 + b2 + c2
-d1 = cc % 10
-ccc = a + b
-d2 = ccc % 10
-print(d1, d2)
+c = ab // 10
+d1 = a2 + b2 + c
+e1 = d1 % 10
+e2 = ab % 10
+print(e1, e2)
