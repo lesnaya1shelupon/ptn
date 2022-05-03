bf50dbf1108550bc1b85cd400155cbc2c3425166
lesnaya1shelupon <tookdd55@gmail.com>
2022-05-03 19:26:57 +0300

    dop.zad

---
+++ hardz_zd.py
@@ -0,0 +1,14 @@
+print("Write nubmers")
+a2 = int(input("a2"))
+a = int(input("a1"))
+b2 = int(input("b2"))
+b = int(input("b1"))
+ab = a + b
+c2 = ab // 10
+a2b2 = a2 + b2 + c2
+c3 = a2b2 // 10
+cc = a2 + b2 + c2
+d1 = cc % 10
+ccc = a + b
+d2 = ccc % 10
+print(d1, d2)
